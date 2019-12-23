29ef9a13b2da0e86a9a3271a7230cbf09997820f
guohuiming <guohuiming@yunliketech.com>
2019-12-23 16:13:55 +0800

    Initial Commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home" @click="clickHandle">
+    <div class="userinfo" @click="bindViewTap">
+      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
+    </div>
+    <!-- 子组件 -->
+    <div class="usermotto">
+      <div class="user-motto">
+        <card :text="motto"></card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: ''
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+  .userinfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    // .userinfo-avatar {
+    //   width: 128rpx;
+    //   height: 128rpx;
+    //   margin: 20rpx;
+    //   border-radius: 50%;
+    // }
+  }
+}
+</style>
